5f3eaa9706c97c83448c9cad9f4fb841b1b51abb
xxxxlc <913847100@qq.com>
2022-05-04 11:11:26 +0800

    2022/05/04

---
+++ Dynamicprogramming/combinationSum4.py
@@ -0,0 +1,25 @@
+# 给你一个由 不同 整数组成的数组 nums ，和一个目标整数 target 。请你从 nums 中找出并返回总和为 target 的元素组合的个数。
+
+# 题目数据保证答案符合 32 位整数范围。
+
+
+class Solution(object):
+    def combinationSum4(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        dp = [1] + [0] * target
+        for i in range(1, target + 1):
+            for num in nums:
+                if num <= i:
+                    dp[i] += dp[i - num]
+        
+        return dp[target]
+
+nums = [1, 2, 3]
+target = 4
+
+a = Solution()
+print(a.combinationSum4(nums, 4))
