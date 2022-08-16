cc878503dd2db1f8f98c9de47ab867220eff21d3
xxxxlc <913847100@qq.com>
2022-08-16 10:02:45 +0800

    '2022/08/16'

---
+++ array/intersection.py
@@ -0,0 +1,16 @@
+# 给定两个数组 nums1 和 nums2 ，返回 它们的交集 。输出结果中的每个元素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        return list(set(nums1).intersection(set(nums2)))
+
+nums1 = [1,2,2,1]
+nums2 = [2,2]
+
+a = Solution()
+print(a.intersection(nums1, nums2))
